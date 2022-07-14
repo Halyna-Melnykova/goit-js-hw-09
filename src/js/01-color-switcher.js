0a03d0f8ea4b02684c21cd66fb081086a7ba997f
Halyna Melnykova <galyamelnykova@gmail.com>
2022-07-14 14:39:16 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  startBtnEl.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtnEl.disabled = true;
+  });
+
+  stopBtnEl.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtnEl.disabled = false;
+  });
