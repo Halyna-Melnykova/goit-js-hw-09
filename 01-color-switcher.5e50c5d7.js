da9c4c8d503a98ce24458453db0ff11f657f7beb
Halyna-Melnykova <104796018+Halyna-Melnykova@users.noreply.github.com>
2022-07-15 15:04:10 +0000

    Deploying to gh-pages from @ Halyna-Melnykova/goit-js-hw-09@98bcfcbe42d97067a092ebf9283af5eac3e238bf 🚀

---
+++ 01-color-switcher.5e50c5d7.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=(document.querySelector("body"),null);t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.5e50c5d7.js.map
